90f3ce9f621fc4e2d2b16170647858a19cdc25cc
Rifqy Fauzan <r1fqyf4uz4n@gmail.com>
2025-06-20 14:05:58 +0700

    fixing animation bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,111 +1,143 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu');
-navToggle = document.getElementById('nav-toggle');
-navClose = document.getElementById('nav-close');
+// NAVIGATION
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
-/* Menu Show */
-if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
 }
 
-/* Menu Hidden */
-if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
-
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link');
-
-const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu');
+if(navClose) {
+  navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu');
+  });
+}
+
+const navLink = document.querySelectorAll('.nav__link');
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*=============== ADD BLUR HEADER ===============*/
+// BLUR HEADER
 const blurHeader = () => {
-    const header = document.getElementById('header');
-    this.scrollY >= 50 ? header.classList.add('blur-header')
-        : header.classList.remove('blur-header')
+  const header = document.getElementById('header');
+  this.scrollY >= 50 ? header.classList.add('blur-header')
+                     : header.classList.remove('blur-header');
 }
 window.addEventListener('scroll', blurHeader);
 
-/*=============== EMAIL JS ===============*/
-const contactForm = document.getElementById('contact-form'),
-    contactMessage = document.getElementById('contact-message');
+// SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+  origin: 'bottom',
+  distance: '50px',
+  duration: 1000,
+  delay: 200,
+  reset: false,
+});
 
-const sendEmail = (e) => {
-    e.preventDefault()
-    // serviceID - templateID - #form - publicKey
-    emailjs.sendForm('service_l9buiso', 'template_stz3x4f', '#contact-form', 'LedVEW-iymax6egIi')
-        .then(() => {
-            // Show sent message
-            contactMessage.textContent = 'Message sent successfully ✅';
+// Common elements
+sr.reveal('.section__title', {
+  delay: 300,
+  origin: 'top'
+});
 
-            // Remove message after five seconds
-            setTimeout(() => {
-                contactMessage.textContent = '';
-            }, 5000);
+// Home section
+sr.reveal('.home__name', {
+  delay: 400,
+  origin: 'left',
+  distance: '80px'
+});
 
-            // Clear input fields
-            contactForm.reset();
-        }, () => {
-            // Show error message
-            contactMessage.textContent = 'Error sending message 😞';
-        })
-}
+sr.reveal('.home__profession', {
+  delay: 500,
+  origin: 'right',
+  distance: '80px'
+});
 
-contactForm.addEventListener('submit', sendEmail);
+// Work section
+sr.reveal('.work__card', {
+  interval: 200,
+  origin: 'bottom'
+});
 
-/*=============== SHOW SCROLL UP ===============*/
+// Info section
+sr.reveal('.about__img', {
+  origin: 'left',
+  distance: '60px'
+});
+
+sr.reveal('.about__content', {
+  origin: 'right',
+  distance: '60px'
+});
+
+// Services section
+sr.reveal('.services__card', {
+  interval: 200,
+  origin: 'bottom'
+});
+
+// Contact section
+sr.reveal('.contact__form', {
+  origin: 'left',
+  distance: '60px'
+});
+
+sr.reveal('.contact__social', {
+  origin: 'right',
+  distance: '60px'
+});
+
+// SCROLL UP
 const scrollUp = () => {
-    const scrollUp = document.getElementById('scroll-up');
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-        : scrollUp.classList.remove('show-scroll')
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                      : scrollUp.classList.remove('show-scroll');
 }
-
 window.addEventListener('scroll', scrollUp);
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+// ACTIVE LINK
+const sections = document.querySelectorAll('section[id]');
 
 const scrollActive = () => {
-    const scrollDown = window.scrollY
+  const scrollDown = window.scrollY;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 58,
-            sectionId = current.getAttribute('id'),
-            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 58,
+          sectionId = current.getAttribute('id'),
+          sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
 
-        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add('active-link')
-        } else {
-            sectionsClass.classList.remove('active-link')
-        }
-    })
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
 }
+window.addEventListener('scroll', scrollActive);
 
-window.addEventListener('scroll', scrollActive)
+// EMAIL JS
+// const contactForm = document.getElementById('contact-form'),
+//       contactMessage = document.getElementById('contact-message');
 
+// const sendEmail = (e) => {
+//   e.preventDefault();
+  
+//   emailjs.sendForm('service_l9buiso', 'template_stz3x4f', '#contact-form', 'LedVEW-iymax6egIi')
+//     .then(() => {
+//       contactMessage.textContent = 'Message sent successfully ✅';
+//       setTimeout(() => {
+//         contactMessage.textContent = '';
+//       }, 5000);
+//       contactForm.reset();
+//     }, () => {
+//       contactMessage.textContent = 'Error sending message 😞';
+//     });
+// }
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2000,
-    delay: 50,
-    reset: true // Animations Repeat
-})
-
-sr.reveal(`.home__data, .experience, .skills, .contact__container`);
-sr.reveal(`.certificate, .section__title, .certificate__card`);
-sr.reveal(`.home__img`, { delay: 600 });
-sr.reveal(`.home__scroll`, { delay: 800 });
-sr.reveal(`.work__card, .services__card`, { interval: 100 });
-sr.reveal(`.about__content`, { origin: 'right' });
-sr.reveal(`.about__img`, { origin: 'left' });
+// contactForm.addEventListener('submit', sendEmail);
